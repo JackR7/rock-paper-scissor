fbb40ec0f47db13adfd70082b612903ac8ae1d7e
JackR7 <apaux7@gmail.com>
2022-11-04 19:21:09 +0100

    add index.css

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function round() {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 1; i++) {
         console.log("Round: " + (i + 1));
         round(playerInput, computerInput);
     }
